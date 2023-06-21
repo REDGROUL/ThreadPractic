a3de4a57b2c8382bc843ef2f7aeacbd79e404108
Admin304 <Admin304@ADMIN304>
2023-06-21 20:17:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Bank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Bank
+    {
+        private int money;
+        public int Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+
+                if (money != value)
+                {
+                    money = value;
+
+                    new Thread(() => {
+
+                        if (File.Exists("hehe.txt"))
+                        {
+                            using (StreamWriter sr = new StreamWriter("hehe.txt"))
+                            {
+                                sr.WriteLine(money);
+                                Console.WriteLine(money);
+
+                            }
+                        }
+                    }).Start();
+                }
+            }
+        }
+
+
+        public int percent;
+
+        public string name;
+        public Bank(int mon, int per, string n)
+        {
+            money = mon;
+            percent = per;
+            name = n;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank(100, 15, "Frodo");
+            Console.WriteLine(b.Money);
+
+            b.Money = 250;
+            Console.ReadLine();
+            
+        }
+    }
+}
